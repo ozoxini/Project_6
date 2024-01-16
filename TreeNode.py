ab636e4f25092bb0418af91cfbc8ef77a40b55b6
ozoxini <kacperozoo@gmail.com>
2024-01-16 23:05:20 +0100

    Utworzenie pliku TreeNode.py

---
+++ TreeNode.py
@@ -0,0 +1,9 @@
+# TreeNode.py
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
