5ebb6cecc215af1627fa94c851ac7ea4dd7a8b92
ozoxini <kacperozoo@gmail.com>
2024-01-16 23:03:58 +0100

    Utworzenie pliku PointData.py

---
+++ PointData.py
@@ -0,0 +1,10 @@
+# PointData.py
+
+class PointData:
+    def __init__(self, date_time, autokonsumpcja, eksport, import_val, pobor, produkcja):
+        self.date_time = date_time
+        self.autokonsumpcja = autokonsumpcja
+        self.eksport = eksport
+        self.import_val = import_val
+        self.pobor = pobor
+        self.produkcja = produkcja
