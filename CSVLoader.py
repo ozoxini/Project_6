a352eeb34e92ec76c2475c70881ff94e887ed8a7
ozoxini <kacperozoo@gmail.com>
2024-01-16 23:06:41 +0100

    Utworzenie pliku CSVLoader.py

---
+++ CSVLoader.py
@@ -0,0 +1,19 @@
+# CSVLoader.py
+import csv
+from PointData import PointData
+from DataAnalyzer import DataAnalyzer
+
+class CSVLoader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.points = []
+
+    def load_data(self):
+        # Implementacja wczytywania danych z pliku CSV
+        # Przeprowadź analizę i zapisz do plików log
+
+    def save_to_binary_file(self, filename):
+        # Implementacja zapisu danych do pliku binarnego
+
+    def load_from_binary_file(self, filename):
+        # Implementacja odczytu danych z pliku binarnego
