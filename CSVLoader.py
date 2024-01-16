72d0bbfc701ae25303bd81daf8f26b998509889a
ozoxini <kacperozoo@gmail.com>
2024-01-16 23:15:22 +0100

    Wprowadzenie poprawek

--- CSVLoader.py
+++ CSVLoader.py
@@ -11,9 +11,12 @@ class CSVLoader:
     def load_data(self):
         # Implementacja wczytywania danych z pliku CSV
         # Przeprowadź analizę i zapisz do plików log
+        pass
 
     def save_to_binary_file(self, filename):
         # Implementacja zapisu danych do pliku binarnego
+        pass
 
     def load_from_binary_file(self, filename):
         # Implementacja odczytu danych z pliku binarnego
+        pass
--- DataAnalyzer.py
+++ DataAnalyzer.py
@@ -7,14 +7,18 @@ class DataAnalyzer:
 
     def sum_autokonsumpcja(self, start_date, end_date):
         # Implementacja sumy autokonsumpcji w danym przedziale czasowym
+        pass
 
     # Pozostałe metody analogicznie
 
     def compare_autokonsumpcja(self, start_date_1, end_date_1, start_date_2, end_date_2):
         # Implementacja porównania autokonsumpcji w dwóch przedziałach
+        pass
 
     def search_records(self, condition, tolerance, start_date, end_date):
         # Implementacja wyszukiwania rekordów spełniających warunek
+        pass
 
     def display_data_in_range(self, start_date, end_date):
         # Implementacja wyświetlania danych w zadanym przedziale
+        pass
