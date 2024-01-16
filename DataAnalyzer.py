25ebc60254ae3071aa5d969ccc3e3c89642ecda1
ozoxini <kacperozoo@gmail.com>
2024-01-16 23:07:26 +0100

    Utworzenie pliku DataAnalyzer.py

---
+++ DataAnalyzer.py
@@ -0,0 +1,20 @@
+# DataAnalyzer.py
+from datetime import datetime
+
+class DataAnalyzer:
+    def __init__(self, points):
+        self.points = points
+
+    def sum_autokonsumpcja(self, start_date, end_date):
+        # Implementacja sumy autokonsumpcji w danym przedziale czasowym
+
+    # Pozostałe metody analogicznie
+
+    def compare_autokonsumpcja(self, start_date_1, end_date_1, start_date_2, end_date_2):
+        # Implementacja porównania autokonsumpcji w dwóch przedziałach
+
+    def search_records(self, condition, tolerance, start_date, end_date):
+        # Implementacja wyszukiwania rekordów spełniających warunek
+
+    def display_data_in_range(self, start_date, end_date):
+        # Implementacja wyświetlania danych w zadanym przedziale
