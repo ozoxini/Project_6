a9b0d066cc920645dd28aaa4d25cc3b65b605142
Jakub2170 <147450529+Jakub2170@users.noreply.github.com>
2024-01-24 13:02:54 +0100

    Delete PointData.py

--- PointData.py
+++
@@ -1,9 +0,0 @@
-# PointData.py
-class PointData:
-    def __init__(self, date_time, autokonsumpcja, eksport, import_val, pobor, produkcja):
-        self.date_time = date_time
-        self.autokonsumpcja = autokonsumpcja
-        self.eksport = eksport
-        self.import_val = import_val
-        self.pobor = pobor
-        self.produkcja = produkcja
